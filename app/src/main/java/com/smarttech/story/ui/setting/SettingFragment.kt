8cb47e73f3b2e5eba9acc7f72b1207f1b22134bf
javavn <javavn2020@gmail.com>
2021-03-01 10:22:08 +0700

    init story reader

---
+++ app/src/main/java/com/smarttech/story/ui/setting/SettingFragment.kt
@@ -0,0 +1,42 @@
+package com.smarttech.story.ui.setting
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import com.smarttech.story.R
+import com.smarttech.story.ui.notifications.NotificationsViewModel
+
+class SettingFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = SettingFragment()
+    }
+
+    private lateinit var viewModel: SettingViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewModel =
+            ViewModelProvider(this).get(SettingViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_setting, container, false)
+        val textView: TextView = root.findViewById(R.id.text_setting)
+        viewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(SettingViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
